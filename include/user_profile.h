7d7e2945843c6891df09789a3e507d57c234fe11
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:23:45 +0530

    Update user_profile.h

--- include/user_profile.h
+++ include/user_profile.h
@@ -1,1 +1,27 @@
+#ifndef USER_PROFILE_H
+#define USER_PROFILE_H
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+// Structure to hold user information
+struct UserProfile {
+    string username;
+    string branch;
+    string skills;
+};
+
+// User Manager for adding and managing profiles
+class UserManager {
+private:
+    unordered_map<string, UserProfile> users; // key = username
+
+public:
+    void addUser(string username, string branch, string skills);
+    void displayUsers();
+    bool userExists(string username);
+};
+
+#endif
 
