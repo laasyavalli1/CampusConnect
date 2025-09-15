d121a0c38a0a8ecc86fd425da763683ea433fac3
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:24:36 +0530

    Update group_management.h

--- include/group_management.h
+++ include/group_management.h
@@ -1,1 +1,20 @@
+#ifndef GROUP_MANAGEMENT_H
+#define GROUP_MANAGEMENT_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class GroupManager {
+private:
+    unordered_map<string, vector<string>> groups; // group name → list of usernames
+
+public:
+    void createGroup(string groupName);
+    void joinGroup(string groupName, string username);
+    void displayGroups();
+};
+
+#endif
 
