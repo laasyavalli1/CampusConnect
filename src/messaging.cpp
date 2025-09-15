1d84b45d85dbafb30e2cfbc6ccba7643dc134968
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:25:51 +0530

    Update messaging.cpp

--- src/messaging.cpp
+++ src/messaging.cpp
@@ -1,1 +1,23 @@
+#include "messaging.h"
+#include <iostream>
+using namespace std;
+
+// Send a message from sender to receiver
+void MessagingSystem::sendMessage(string sender, string receiver, string message) {
+    inbox[receiver].push("From " + sender + ": " + message);
+    cout << "Message sent to " << receiver << " successfully.\n";
+}
+
+// Read all messages for a user
+void MessagingSystem::readMessages(string username) {
+    cout << "\n--- Messages for " << username << " ---\n";
+    if (inbox.find(username) == inbox.end() || inbox[username].empty()) {
+        cout << "No messages.\n";
+        return;
+    }
+    while (!inbox[username].empty()) {
+        cout << inbox[username].front() << endl;
+        inbox[username].pop();
+    }
+}
 
