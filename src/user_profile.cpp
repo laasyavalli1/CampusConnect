25080375ed07b4ab8948f125d54781b66f35be97
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:24:09 +0530

    Update user_profile.cpp

--- src/user_profile.cpp
+++ src/user_profile.cpp
@@ -1,1 +1,31 @@
+#include "user_profile.h"
+#include <iostream>
+using namespace std;
+
+// Add a new user profile
+void UserManager::addUser(string username, string branch, string skills) {
+    if (users.find(username) != users.end()) {
+        cout << "User already exists!\n";
+        return;
+    }
+
+    UserProfile newUser = {username, branch, skills};
+    users[username] = newUser;
+    cout << "User " << username << " added successfully.\n";
+}
+
+// Display all users
+void UserManager::displayUsers() {
+    cout << "\n--- Registered Users ---\n";
+    for (auto &entry : users) {
+        cout << "Username: " << entry.second.username
+             << " | Branch: " << entry.second.branch
+             << " | Skills: " << entry.second.skills << endl;
+    }
+}
+
+// Check if user exists
+bool UserManager::userExists(string username) {
+    return users.find(username) != users.end();
+}
 
