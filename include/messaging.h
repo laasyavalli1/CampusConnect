0d92c39a64d1b6f3b68ac925d6ad8ebc0eadcc8b
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:25:26 +0530

    Update messaging.h

--- include/messaging.h
+++ include/messaging.h
@@ -1,1 +1,20 @@
+#ifndef MESSAGING_H
+#define MESSAGING_H
+
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class MessagingSystem {
+private:
+    unordered_map<string, queue<string>> inbox; // username → messages
+
+public:
+    void sendMessage(string sender, string receiver, string message);
+    void readMessages(string username);
+};
+
+#endif
 
