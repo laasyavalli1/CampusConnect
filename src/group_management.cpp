47db7bf4f3d72e4095b00d777f37599ac574a618
laasyavalli1 <laasyavalli1@gmail.com>
2025-09-15 12:24:59 +0530

    Update group_management.cpp

--- src/group_management.cpp
+++ src/group_management.cpp
@@ -1,1 +1,36 @@
+#include "group_management.h"
+#include <iostream>
+using namespace std;
+
+// Create a new group
+void GroupManager::createGroup(string groupName) {
+    if (groups.find(groupName) != groups.end()) {
+        cout << "Group already exists!\n";
+        return;
+    }
+    groups[groupName] = {};
+    cout << "Group '" << groupName << "' created successfully.\n";
+}
+
+// Add user to a group
+void GroupManager::joinGroup(string groupName, string username) {
+    if (groups.find(groupName) == groups.end()) {
+        cout << "Group does not exist!\n";
+        return;
+    }
+    groups[groupName].push_back(username);
+    cout << username << " joined group " << groupName << ".\n";
+}
+
+// Display all groups and members
+void GroupManager::displayGroups() {
+    cout << "\n--- Groups ---\n";
+    for (auto &entry : groups) {
+        cout << "Group: " << entry.first << "\nMembers: ";
+        for (auto &member : entry.second) {
+            cout << member << " ";
+        }
+        cout << endl;
+    }
+}
 
