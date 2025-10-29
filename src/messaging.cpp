f79bb7bc98f2d58e87fbd9792130330affd66360
Pradeeshwaran M <pradeeshwaran2006@gmail.com>
2025-10-29 21:47:26 +0530

    Update messaging.cpp

--- src/messaging.cpp
+++ src/messaging.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 void messagingsystem::sendmessage(const string& sender, const string& receiver) {
-    cin.ignore();  // Clear input buffer before taking new input
+    cin.ignore();  
     message m;
     m.sender = sender;  // Set sender's email
     m.receiver = receiver;  // Set receiver's email
     cout << "enter message: ";
     getline(cin, m.text);  // Get the message text from user input
     messages.push_back(m);  // Add the message to the list of messages
-    cout << " message sent!\n";  // Confirmation message
+    cout << " message sent!\n";  
 }
 
 void messagingsystem::viewmessages(const string& sender, const string& receiver) {
@@ -21,6 +21,6 @@ void messagingsystem::viewmessages(const string& sender, const string& receiver)
     for (auto& m : messages) {
         if ((m.sender == sender && m.receiver == receiver) ||
             (m.sender == receiver && m.receiver == sender))
-            cout << m.sender << ": " << m.text << endl;  // Display message content
+            cout << m.sender << ": " << m.text << endl;  
     }
 }
