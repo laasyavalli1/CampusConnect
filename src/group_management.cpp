efd2341f0574e86d93e1b89f6482d41a8ff78e1b
Pradeeshwaran M <pradeeshwaran2006@gmail.com>
2025-10-29 22:27:56 +0530

    Update group_management.cpp

--- src/group_management.cpp
+++ src/group_management.cpp
@@ -1,38 +1,39 @@
-// This file implements the `groupmanager` class, which manages group creation and listing. 
-// It allows users to create new groups by specifying a name and members, as well as listing all groups and their members.
-
+// The groupmanager class handles the creation and listing of groups within the platform.
 #include "../include/group_management.h"
 #include <iostream>
 using namespace std;
 
+// Creates a new group by asking the user for the group name and members' emails
 void groupmanager::creategroup() {
     cin.ignore();  
-    group g;
-    cout << "enter group name: ";
+    group g;       
+    cout << "Enter group name: ";
     getline(cin, g.name);  
+
     int n;
-    cout << "enter number of members: ";
-    cin >> n;  // Get number of members in the group
+    cout << "Enter number of members: ";
+    cin >> n;  // Get the number of members
     cin.ignore();  
 
-    // Loop to get member emails and add them to the group
+    // Collects the emails of the group members
     for (int i = 0; i < n; i++) {
         string email;
-        cout << "enter member " << i + 1 << " email: ";
-        getline(cin, email);  // Get each member's email
-        g.members.push_back(email);  // Add email to the group's member list
+        cout << "Enter member " << i + 1 << " email: ";
+        getline(cin, email);  // Get the email for each member
+        g.members.push_back(email);  // Add the member to the group
     }
-    
-    groups.push_back(g);  // Add the new group to the list of groups
-    cout << " group created successfully!\n";  
+
+    groups.push_back(g);  // Add the new group to the groups list
+    cout << "Group created successfully!\n";  
 }
 
+// Lists all the groups and their members
 void groupmanager::listgroups() {
-    cout << "\ngroups:\n";
-    // Loop through and list each group and its members
+    cout << "\nGroups:\n";
+    // Iterates through all groups and displays their name and members
     for (auto& g : groups) {
-        cout << "- " << g.name << " [ ";  // Print group name
-        for (auto& m : g.members) cout << m << " ";  // Print each member's email
-        cout << "]\n";  
+        cout << "- " << g.name << " [ ";
+        for (auto& m : g.members) cout << m << " ";  // List each member of the group
+        cout << "]\n";
     }
 }
