e66565bb178664158a7daf4937e80e31190b0eef
Pradeeshwaran M <pradeeshwaran2006@gmail.com>
2025-10-29 21:40:50 +0530

    Update group_management.h

--- include/group_management.h
+++ include/group_management.h
@@ -1,3 +1,6 @@
+// This header file defines a `group` structure with a name and a list of members, 
+// and a `groupmanager` class that allows for creating and listing groups.
+
 #ifndef group_management_h
 #define group_management_h
 
@@ -7,17 +10,17 @@
 using namespace std;
 
 struct group {
-    string name;
-    vector<string> members;
+    string name;           // Name of the group
+    vector<string> members; // List of members in the group
 };
 
 class groupmanager {
 private:
-    vector<group> groups;
+    vector<group> groups; // Container to hold all the groups
 
 public:
-    void creategroup();
-    void listgroups();
+    void creategroup();    // Function to create a new group
+    void listgroups();     // Function to list all the groups
 };
 
 #endif
