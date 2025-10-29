7de39eef29e92c4f5d6839721065322e8df19024
Saravana Kumar V <saravananitt7@gmail.com>
2025-10-29 11:51:30 +0530

    Update navigation.h

--- navigation.h
+++ navigation.h
@@ -9,22 +9,22 @@ using namespace std;
 
 // Structure to represent each building
 struct Building {
-    string name;
-    string description;
-    vector<string> connectedTo;  // adjacency list
+    string name;
+    string description;
+    vector<string> connectedTo;  // adjacency list
 };
 
 // Graph class for inside-campus navigation
 class CampusMap {
 private:
-    unordered_map<string, Building> graph;
+    unordered_map<string, Building> graph;
 
 public:
-    void addBuilding(const string &name, const string &desc);
-    void addPath(const string &from, const string &to);
-    bool buildingExists(const string &name) const;
-    void showBuildingDetails(const string &name) const;
-    void listAllBuildings() const;
+    void addBuilding(const string &name, const string &desc);
+    void addPath(const string &from, const string &to);
+    bool buildingExists(const string &name) const;
+    void showBuildingDetails(const string &name) const;
+    void listAllBuildings() const;
 };
 
 #endif
